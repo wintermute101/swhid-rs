05d0883bffb8299658efbea1cfbeeced17f94179
Valentin Lorentz <vlorentz@softwareheritage.org>
2025-11-13 12:28:17 +0100

    Add test for release from Git tag

--- tests/git.rs
+++ tests/git.rs
@@ -4,6 +4,7 @@ use assert_fs::prelude::*;
 use git2::{Repository, Signature, Time};
 
 use swhid::git::*;
+use swhid::release::{ObjectType, Release};
 use swhid::revision::Revision;
 
 fn bs(s: &'static str) -> Box<[u8]> {
@@ -31,7 +32,10 @@ fn test_revision_swhid() {
         .add_path(file_path.path().strip_prefix(tmp.path()).unwrap())
         .unwrap();
     let tree_oid = index.write_tree().unwrap();
-    let tree_hash = hex::decode("0efb37b28c53c7e4fbd253bb04a4df14008f63fe").unwrap().try_into().unwrap();
+    let tree_hash = hex::decode("0efb37b28c53c7e4fbd253bb04a4df14008f63fe")
+        .unwrap()
+        .try_into()
+        .unwrap();
     assert_eq!(oid_to_array(tree_oid), tree_hash);
     let tree = repo.find_tree(tree_oid).unwrap();
 
@@ -75,5 +79,73 @@ fn test_revision_swhid() {
     // CoreSWHID.from_string('swh:1:rev:07cde6575fb633ef9b5ecbe730e6eb97475a2fd9')
 
     let swhid = revision_swhid(&repo, &commit_oid).unwrap();
-    assert_eq!(swhid.to_string(), "swh:1:rev:07cde6575fb633ef9b5ecbe730e6eb97475a2fd9");
+    assert_eq!(
+        swhid.to_string(),
+        "swh:1:rev:07cde6575fb633ef9b5ecbe730e6eb97475a2fd9"
+    );
+}
+
+#[test]
+fn test_release_swhid() {
+    let tmp = assert_fs::TempDir::new().unwrap();
+    let repo = Repository::init(tmp.path()).unwrap();
+
+    // Create content
+    let mut index = repo.index().unwrap();
+    let file_path = tmp.child("test.txt");
+    file_path.write_str("test content").unwrap();
+
+    // Create directory
+    index
+        .add_path(file_path.path().strip_prefix(tmp.path()).unwrap())
+        .unwrap();
+    let tree_oid = index.write_tree().unwrap();
+    let tree_hash = hex::decode("0efb37b28c53c7e4fbd253bb04a4df14008f63fe")
+        .unwrap()
+        .try_into()
+        .unwrap();
+    assert_eq!(oid_to_array(tree_oid), tree_hash);
+    let tree = repo.find_tree(tree_oid).unwrap();
+
+    // Create tag
+    let sig = Signature::new("Test User", "test@example.com", &Time::new(1763027354, 60)).unwrap();
+    let tag_oid = repo
+        .tag(
+            "v1.0",
+            &tree.into_object(),
+            &sig,
+            "Test tag",
+            /* force= */ false,
+        )
+        .unwrap();
+
+    let rev = release_from_git(&repo, &tag_oid).unwrap();
+    assert_eq!(
+        rev,
+        Release {
+            object: tree_hash,
+            object_type: ObjectType::Directory,
+            name: bs("v1.0"),
+            author: Some(bs("Test User <test@example.com>")),
+            author_timestamp: Some(1763027354),
+            author_timestamp_offset: Some(bs("+0100")),
+            extra_headers: Vec::new(),
+            message: Some(bs("Test tag")),
+        }
+    );
+
+    // Checked against the implementation in https://archive.softwareheritage.org/swh:1:dir:60e683f48069373ee85227f2d7ab2eb1a8873ddb;origin=https://gitlab.softwareheritage.org/swh/devel/swh-model.git;visit=swh:1:snp:291aefbdccd43abac57629431201c2fd55284df7;anchor=swh:1:rev:9e54500902fc00ab1e6400431e2803b9bb41cc0a
+    // using this script:
+    // >>> from swh.model.model import *
+    // >>> person = Person.from_fullname(b"Test User <test@example.com>")
+    // >>> ts = TimestampWithTimezone(timestamp=Timestamp(seconds=1763027354, microseconds=0), offset_bytes=b"+0100")
+    // >>> rel = Release(name=b"v1.0", target_type=ObjectType.DIRECTORY, target=bytes.fromhex("0efb37b28c53c7e4fbd253bb04a4df14008f63fe"), message=b"Test tag", author=person, date=ts, synthetic=False)
+    // >>> rel.swhid()
+    // CoreSWHID.from_string('swh:1:rel:46d326edb8bfc49b757ccd09930365595806bfc0')
+
+    let swhid = release_swhid(&repo, &tag_oid).unwrap();
+    assert_eq!(
+        swhid.to_string(),
+        "swh:1:rel:46d326edb8bfc49b757ccd09930365595806bfc0"
+    );
 }
